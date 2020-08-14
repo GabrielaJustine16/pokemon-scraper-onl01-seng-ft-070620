d9828ca97beaead1bf71357be5fab1dfc3672d6e
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-14 21:05:35 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@
     VALUES (?,?)
     SQL
     DB[:conn].execute(sql,self.name,self.type,self.db)
-    @id=DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon;")[0][0]
+    
   end 
   
    def self.find(id, db)
